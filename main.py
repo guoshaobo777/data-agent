620247f3910714ac6a5d3dec4b76a215da038a5d
xinlin.chen <xinlinchen@6tiantian.com>
2026-03-16 21:23:36 +0800

    load config file

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# 这是一个示例 Python 脚本。
+from pathlib import Path
 
-# 按 Alt+R 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+_project_path = Path(__file__).parent
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+def get_project_path() ->  Path:
+    return _project_path
 
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
